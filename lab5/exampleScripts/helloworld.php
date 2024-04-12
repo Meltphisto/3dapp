0d59f343dcc6a286bc7ee0bf98919c7743ed3e3d
Linyu Wang <linyu.wang19@student.xjtlu.edu.cn>
2024-04-12 19:40:20 +0800

    lab5 fin

---
+++ lab5/exampleScripts/helloworld.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>hellow world</title>
+    </head>
+    <body>
+        <p>html says hellow world</p>
+            <script type="text/javascript">
+            document.write('<p>javascript sasys hellow world</p>');
+        </script>
+        <?php
+            echo "<p>php says hellow world</p>";
+        ?>
+    </body>
+</html>
