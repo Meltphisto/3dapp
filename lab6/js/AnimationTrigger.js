a16fc88f599f0869a0652e0d39337d00e9fb6e3a
Linyu Wang <linyu.wang19@student.xjtlu.edu.cn>
2024-05-14 20:54:16 +0800

    lab 6 debug

--- lab6/js/AnimationTrigger.js
+++ lab6/js/AnimationTrigger.js
@@ -3,20 +3,20 @@ var animating = false;
 function AnimationOn()
 {
     animating = !animating;
-    document.getElementById('model_RotationTimer').setAttribute('enabled',animating.toString());
+    document.getElementById('model__RotationTimer').setAttribute('enabled',animating.toString());
     
 }
 
 function AnimationOff()
 {
     animating = false;
-    document.getElementById('model_RotationTimer').setAttribute('enabled',animating.toString())
+    document.getElementById('model__RotationTimer').setAttribute('enabled',animating.toString())
 }
 
 function AnimationModel()
 {
-    if(document.getElementById('model_RotationTimer').getAttribute('enabled') != true)
-        document.getElementById('model_RotationTimer').setAttribute('enabled','true');
+    if(document.getElementById('model__RotationTimer').getAttribute('enabled') != true)
+        document.getElementById('model__RotationTimer').setAttribute('enabled','true');
     else
-        document.getElementById('model_RotationTimer').setAttribute('enabled','false');
+        document.getElementById('model__RotationTimer').setAttribute('enabled','false');
 }
